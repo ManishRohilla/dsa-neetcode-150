50852f4fd3564495bb96874d935f7ce95b43066b
ManishRohilla <63550539+ManishRohilla@users.noreply.github.com>
2026-04-19 09:49:32 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] state = new int[26];
+        for(int i =0; i< 26; i++){
+            state[i] = 0;
+        }
+        for(char c: s.toCharArray()){
+            state[c - 'a']++;
+        }
+        for(char c: t.toCharArray()){
+            state[c - 'a']--;
+        }
+
+        for (int i = 0; i<26; i++){
+            if(state[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
