d55cbdd8842faed71471fa776207d8d113697b61
ManishRohilla <63550539+ManishRohilla@users.noreply.github.com>
2026-04-25 15:00:15 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int buy = prices[0];
+       int maxProfit = 0 ;
+       for (int i = 1; i < prices.length; i++){
+        if(prices[i]<buy){
+            buy = prices[i];
+        }
+        maxProfit = Math.max(prices[i]-buy, maxProfit);
+       }
+       return maxProfit;
+    }
+}
